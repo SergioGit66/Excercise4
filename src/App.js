756e63526c9f9866ceb90d7867e3003bbabcf4a6
SergioGit66 <sergiodomingueztenorio66@gmail.com>
2023-06-29 15:45:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Nav">
+            <h1> React Exercises</h1>
+            <div className="numberExs">
+                <h3>#1</h3>
+                <h3>#2</h3>
+                <h3>#3</h3>
+                <h3>#4</h3>
+            </div>
+
+        </div>
+        <div className="exercise">Exercise 3</div>
+        <div className="textInfo">
+            <p><u>Iteration 1:</u>Give the value of "x"from ComponentA to ComponentC</p>
+          
+            <p><u>Iteration 2:</u>Change the "x" when clicking on the button "x++"</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
